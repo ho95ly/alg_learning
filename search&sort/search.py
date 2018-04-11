559b43906e644b5e623889925fda3612988720f1
ho95ly <hhabc1995@qq.com>
2018-04-11 09:59:07 +0800

    # learning search.

--- search&sort/search.py
+++ search&sort/search.py
@@ -3,7 +3,16 @@ __author__ = 'h0ly95'
 import numpy as np
 
 
-def baniarySearch(num_seq, num):
+def SequenceSearch(arr, num):
+    length = len(arr)
+    pos = []
+    for i in range(length):
+        if arr[i] == num:
+            pos.append(i)
+    return pos
+
+
+def binarySearch(num_seq, num):  # element must be sorted.
 
     len_seq = len(num_seq)
     left_cursor = 0
@@ -24,8 +33,70 @@ def baniarySearch(num_seq, num):
     return False
 
 
+def binarySearch2(arr, num, left_cursor, right_cursor):
+    if left_cursor > right_cursor:
+        return -1
+    middle_cursor = int((left_cursor+right_cursor)/2)
+    if a[middle_cursor] == num:
+        return middle_cursor
+    elif a[middle_cursor] < num:
+        return binarySearch2(arr, num, middle_cursor+1, right_cursor)
+    elif a[middle_cursor] > num:
+        return binarySearch2(arr, num, left_cursor, middle_cursor-1)
+
+
+def insertionSearch(arr, num, left_cursor, right_cursor):
+    if left_cursor > right_cursor:
+        return -1
+    middle_cursor = int(left_cursor+(num-arr[left_cursor])/(arr[right_cursor]-arr[left_cursor])
+                        * (right_cursor-left_cursor))
+    if a[middle_cursor] == num:
+        return middle_cursor
+    elif a[middle_cursor] < num:
+        return binarySearch2(arr, num, middle_cursor + 1, right_cursor)
+    elif a[middle_cursor] > num:
+        return binarySearch2(arr, num, left_cursor, middle_cursor - 1)
+
+
+def Fibonacci(len=20):
+
+    F = np.zeros(20)
+    F[0] = 0
+    F[1] = 1
+    for i in range(2, len):
+        F[i] = F[i-1] + F[i-2]
+
+    return F
+
+
+def FibonacciSearch(arr, num):
+    length = len(arr)
+    left_cursor = 0
+    right_cursor = length-1
+    F = Fibonacci()
+    k = 0
+    while length > F[k]:
+        k = k+1
+    tmp_arr = np.zeros(F[k]-1)
+    tmp_arr[0:length] = arr
+
+    while left_cursor <= right_cursor:
+        middle_cursor = left_cursor+F[k-1]-1
+        if num < tmp_arr[middle_cursor]:
+            right_cursor = middle_cursor - 1
+            k = k-1
+        elif num > tmp_arr[middle_cursor]:
+            left_cursor = middle_cursor + 1
+            k = k-2
+        else:
+            if middle_cursor < length:
+                return middle_cursor
+            else:
+                return length-1
+    return False
+
+
+
 if __name__ == '__main__':
-    a = np.arange(12)
-    b = 4
-    c = baniarySearch(a, b)
+    pass
 
